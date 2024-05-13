459247d41994ebd80003741b7a02c7d663403e3a
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-13 16:36:31 -0400

    staff page

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,8 +1,30 @@
+import { contactUsData as data } from './contactUsData'
 import './ContactUs.css'
 
 const ContactUs = () => {
+    const socialMediaKeys = Object.keys(data.socialMedia)
+    
     return <>
-        <h1>ContactUs</h1>
+        <main>
+            <h1>Contact Us</h1>
+            <div id='socials-container'>
+                <h2>Social Media</h2>
+                <img src="https://t4.ftcdn.net/jpg/02/85/44/69/240_F_285446974_yVsizh73QxHP5QeI1ghJC8Vd5ikhSpxH.jpg" alt="socials" />
+                <ul className="socials">
+                    {socialMediaKeys.map( (type, index) => (
+                        <li key={index}>
+                            <p><b>{type}</b></p>
+                            <a href="http://">{data.socialMedia[type]}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <ul>
+                <li><b>Phone:</b> {data.phoneNumber}</li>
+                <li><b>Email:</b> {data.email}</li>
+                <li><b>Address:</b> {data.address}</li>
+            </ul>
+        </main>
     </>
 }
 
---
+++ src/components/ContactUs/contactUsData.jsx
@@ -0,0 +1,13 @@
+const contactUsData = {
+    phoneNumber: "+1 (555) 123-4567",
+    email: "info@luxeroyalecasino.com",
+    address: "123 High Roller Blvd, Las Vegas, NV",
+    socialMedia: {
+        facebook: "@LuxeRoyaleCasino",
+        twitter: "@LuxeRoyaleVegas",
+        instagram: "@LuxeRoyale",
+        youtube: "Luxe Royale Casino"
+    }
+}
+
+export { contactUsData }
